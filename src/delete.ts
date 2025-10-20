3f60e8f391ba1b622eebd420db752fa8ce802980
shakib-chahealth <mauijaloskor@gmail.com>
2025-10-20 10:59:04 +0600

    Delete done

---
+++ src/delete.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const deletes = async () => {
+  // Delete single data
+  //   const singleDelete = await prisma.post.delete({
+  //     where: {
+  //       id: 2,
+  //     },
+  //   });
+
+  // Delete multiple data
+  const deleteMany = await prisma.post.deleteMany({
+    where: {
+      published: true,
+    },
+  });
+
+  console.log(deleteMany);
+};
+
+deletes();
